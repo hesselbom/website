47ba1b2bba4c433211a29c639de0345c4358bbf6
Viktor Hesselbom <viktor@hesselbom.net>
2020-08-31 20:52:02 +0200

    Fix opacity hover issue

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import s from './Card.css'
 
 export default ({ title, img, year, ...props }) => (
-  <a {...props}>
+  <a {...props} class={s.link}>
     <article style={{ backgroundImage: `url(${img})` }} class={s.card}>
       <div class={s.inner}>
         <h2 class={s.title}>{title}</h2>
