b262eb0c3cec54d301d7745e8823f0a8b58b69ac
Viktor Hesselbom <viktor@hesselbom.net>
2020-11-18 16:51:43 +0100

    Add Podger privacy policy

--- src/routes/PrivacyList.js
+++ src/routes/PrivacyList.js
@@ -5,9 +5,10 @@ export default () => (
   <div class={s.view}>
     <h1>Privacy Policies</h1>
     <ul>
+      <li><a href='/privacy-policy/fish-in-a-watch'>Fish in a Watch</a></li>
+      <li><a href='/privacy-policy/podger'>Podger</a></li>
       <li><a href='/privacy-policy/shufflepong'>Shufflepong</a></li>
       <li><a href='/privacy-policy/tubesnake'>Tubesnake</a></li>
-      <li><a href='/privacy-policy/fish-in-a-watch'>Fish in a Watch</a></li>
     </ul>
     <BackFooter />
   </div>
