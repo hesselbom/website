f7bf68dc86e2ca53b0862052a39f52e27a7ff227
Viktor Hesselbom <viktor@hesselbom.net>
2021-08-18 09:29:26 +0200

    Add more projects

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
 import s from './Card.css'
 
-export default ({ title, img, year, ...props }) => (
+export default ({ title, img, year, platform, ...props }) => (
   <a {...props} class={s.link}>
     <article style={{ backgroundImage: `url(${img})` }} class={s.card}>
       <div class={s.inner}>
         <h2 class={s.title}>{title}</h2>
-        <time class={s.date} datetime={year}>{year} - Apple Watch</time>
+        <time class={s.date} datetime={year}>{year} - {platform}</time>
       </div>
     </article>
   </a>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -11,22 +11,39 @@ export default () => (
     <h2>Web, iOS, Android, Apple Watch Developer</h2>
     <Grid>
       <Card
+        title='Shaky Shapes'
+        img={require('../images/shakyshapes.png').default}
+        href='https://apps.apple.com/us/app/shaky-shapes/id1528137086'
+        year='2021'
+        platform='Apple Watch, iOS'
+      />
+      <Card
+        title='Podger'
+        img={require('../images/podger.png').default}
+        href='https://apps.apple.com/us/app/podger-podcast-player/id1540792803'
+        year='2020'
+        platform='Apple Watch, iOS'
+      />
+      <Card
         title='Shufflepong'
         img={require('../images/shufflepong.png').default}
         href='https://apps.apple.com/us/app/shufflepong/id1527268585'
         year='2020'
+        platform='Apple Watch, iOS'
       />
       <Card
         title='Tubesnake'
         img={require('../images/tubesnake.png').default}
         href='https://apps.apple.com/se/app/tubesnake/id1484342417'
         year='2020'
+        platform='Apple Watch, iOS'
       />
       <Card
         title='Fish in a Watch'
         img={require('../images/fishinawatch.png').default}
         href='https://apps.apple.com/us/app/fish-in-a-watch/id1454215856'
         year='2019'
+        platform='Apple Watch, iOS'
       />
     </Grid>
     <Footer />
