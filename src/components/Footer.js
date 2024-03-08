3eeed7876915189ad48435e471a6b870c31e09c4
Viktor Hesselbom <viktor@hesselbom.net>
2024-03-08 14:27:50 +0100

    Update twitter username

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ export default () => {
     <footer class={s.footer}>
       <p class={s.contact}>Contact me at <a href={mail}>viktor@hesselbom.net</a></p>
       <p><a rel='nofollow' href='https://www.youtube.com/channel/UCwesYXkLUWs5ykr6H5AEauw'>YouTube</a></p>
-      <p><a rel='nofollow' href='https://twitter.com/mrhesselbom'>Twitter</a></p>
+      <p><a rel='nofollow' href='https://twitter.com/hesselbom'>Twitter / 𝕏</a></p>
       <p><a rel='nofollow' href='https://www.linkedin.com/in/hesselbom/'>LinkedIn</a></p>
       <p class={s.policies}><a href='/privacy-policy'>App Privacy Policies</a></p>
       <p class={s.policies}>Hesselbom Tech AB</p>
