b2ce957e358dc67ddc02ae480f9b48d0ab86dca1
Viktor Hesselbom <viktor@hesselbom.net>
2024-05-31 14:55:29 +0200

    Add mastodon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,10 @@ export default () => {
   return (
     <footer class={s.footer}>
       <p class={s.contact}>Contact me at <a href={mail}>viktor@hesselbom.net</a></p>
-      <p><a rel='nofollow' href='https://www.youtube.com/channel/UCwesYXkLUWs5ykr6H5AEauw'>YouTube</a></p>
-      <p><a rel='nofollow' href='https://twitter.com/hesselbom'>Twitter / 𝕏</a></p>
-      <p><a rel='nofollow' href='https://www.linkedin.com/in/hesselbom/'>LinkedIn</a></p>
+      <p><a rel='me' href='https://www.youtube.com/channel/UCwesYXkLUWs5ykr6H5AEauw'>YouTube</a></p>
+      <p><a rel='me' href='https://twitter.com/hesselbom'>Twitter / 𝕏</a></p>
+      <p><a rel='me' href='https://www.linkedin.com/in/hesselbom/'>LinkedIn</a></p>
+      <p><a rel='me' href='https://mastodon.social/@viktorhesselbom'>Mastodon</a></p>
       <p class={s.policies}><a href='/privacy-policy'>App Privacy Policies</a></p>
       <p class={s.policies}>Hesselbom Tech AB</p>
     </footer>
