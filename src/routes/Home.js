be8b3cdd545885db3c4b85a520034e139f62bb26
Viktor Hesselbom <viktor@hesselbom.net>
2025-12-06 14:19:47 +0100

    Add aso suite to start page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -11,6 +11,13 @@ export default () => (
     <h2>Web, iOS, Android, Apple Watch Developer</h2>
     <Grid>
       <Card
+        title='ASO Suite'
+        img={require('../images/asosuite.png').default}
+        href='https://www.asosuite.com/'
+        year='2024'
+        platform='Website'
+      />
+      <Card
         title='Watchletic'
         img={require('../images/lubba.png').default}
         href='https://www.watchletic.com/'
